95e0a2b05b6adb6fc266da6cf3e4f4a8e83272ec
mroger25 <madsonmath@hotmail.com>
2021-08-19 23:02:08 -0300

    Final

--- Snowflake.js
+++ Snowflake.js
@@ -3,58 +3,63 @@ const drawLeg = (e, x, y, ang, length, width) => {
   e.lineWidth = width;
   e.lineCap = "round";
   e.translate(x, y);
-  e.moveTo(0, 0);
   e.rotate(ang);
+  e.moveTo(0, -length);
   e.lineTo(0, length);
   e.stroke();
   e.rotate(-ang);
   e.translate(-x, -y);
 };
 
-const drawFlake = (e, x, y, r, n, color) => {
-  const angLeg = Math.PI * (2 / n);
+const drawFlake = (e, x, y, r, angle, color) => {
+  const a = angle || 0;
+  const angLeg = Math.PI * (2 / 3);
   e.strokeStyle = color;
-  for (let i = 0; i < n; i++) {
-    const leg = i * angLeg;
+  for (let i = 0; i < 3; i++) {
+    const leg = i * angLeg + a;
     drawLeg(e, x, y, leg, r * 0.8, r * 0.07);
   }
 };
 
 export class Snowflake {
   constructor() {
-    this.pos = { x: Math.random() * 600, y: Math.random() - 11 };
-    this.vel = { x: 0, y: 0 };
-    this.acc = { x: 0, y: 0 };
-    this.r = Math.random() * 2;
-    this.totalLegs = 3 + Math.floor(Math.random() * 6);
+    this.r = 0.25 + Math.random() * 2;
     this.mass = this.r * this.r * Math.PI;
+    this.pos = { x: Math.random() * window.innerWidth, y: Math.random() - 11 };
+    this.vel = { x: 0, y: 0 };
+    this.acc = { x: 0, y: 0.001 * this.mass };
+    this.angle = Math.random() * 2 * Math.PI;
+    this.vel_ang = (Math.PI / 180) * (1 - Math.random() * 2);
   }
 
   offScreen() {
-    return this.pos.y > 600 + this.r;
+    return (
+      this.pos.x - this.r > window.innerWidth ||
+      this.pos.x + this.r < 0 ||
+      this.pos.y - this.r > window.innerHeight
+    );
   }
 
-  applyForce(force) {
-    this.acc.x = force.x * this.mass;
-    this.acc.y = force.y * this.mass;
+  mcu() {
+    this.angle += this.vel_ang;
   }
 
-  fhv(vo, a) {
-    return vo + a;
+  fhv() {
+    this.vel = { x: this.vel.x + this.acc.x, y: this.vel.y + this.acc.y };
   }
 
-  fhp(so, vo, a) {
-    return so + vo + a / 2;
+  fhp() {
+    this.pos = { x: this.pos.x + this.vel.x, y: this.pos.y + this.vel.y };
   }
 
   update() {
-    this.vel.x = this.fhv(this.vel.x, this.acc.x);
-    this.vel.y = this.fhv(this.vel.y, this.acc.y);
-    this.pos.x = this.fhp(this.pos.x, this.vel.x, this.acc.x);
-    this.pos.y = this.fhp(this.pos.y, this.vel.y, this.acc.y);
+    this.acc.x = Math.cos(Math.random() * Math.PI) / 100;
+    this.mcu();
+    this.fhv();
+    this.fhp();
   }
 
   render(e) {
-    drawFlake(e, this.pos.x, this.pos.y, this.r * 5, this.totalLegs, "#FFF");
+    drawFlake(e, this.pos.x, this.pos.y, this.r * 5, this.angle, "#FFF");
   }
 }
--- main.js
+++ main.js
@@ -3,20 +3,14 @@ import { Snowflake } from "./Snowflake.js";
 
 class Sketch {
   constructor() {
-    this.myCanvas = new CanvasActuator(600, 600, "#000");
+    this.myCanvas = new CanvasActuator(window.innerWidth, window.innerHeight, "#000");
     this.myCanvas.on("draw", this.draw.bind(this));
     this.snow = [];
-    this.gravity = { x: 0, y: 0.001 };
-
-    // this.myCanvas = new CanvasActuator(600, 600, "#000");
-    // this.myCanvas.on("draw", this.draw.bind(this));
-    // this.snow = new Snowflake();
   }
 
   draw() {
     this.snow.push(new Snowflake());
     for (const flake of this.snow) {
-      flake.applyForce(this.gravity);
       flake.update();
       flake.render(this.myCanvas.ctx);
     }
@@ -27,8 +21,6 @@ class Sketch {
         this.snow.splice(i, 1);
       }
     }
-
-    // this.snow.render(this.myCanvas.ctx);
   }
 }
 
